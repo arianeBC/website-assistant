48b937f06c84f5c42a08f44780dae16f13f21840
Ariane <arianebrosseaucote@gmail.com>
2024-08-26 11:20:42 +1100

    fix: Edit openai embedding

--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -1,7 +1,10 @@
-import requests
 import json
 import os
+
+import requests
 from openai import OpenAI
+
+from app.utils.helper_functions import build_prompt, construct_messages_list
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 openai_client = OpenAI(api_key=OPENAI_API_KEY)
@@ -10,29 +13,45 @@ OPENAI_EMBEDDING_MODEL = 'text-embedding-ada-002'
 PROMPT_LIMIT = 3750
 CHATGPT_MODEL = 'gpt-4-1106-preview'
 
+
 def get_embedding(chunk):
-    response = openai_client.embeddings.create(
-        input=chunk, model=OPENAI_EMBEDDING_MODEL)
-    embedding = response.data[0].embedding
-    return embedding
-
-def get_llm_answer(prompt):
-    messages = [{"role": "system", "content": "You are a helpful assistant."}]
-    messages.append({"role": "user", "content": prompt})
-
-    url = 'https://api.openai.com/v1/chat/completions'
+    url = 'https://api.openai.com/v1/embeddings'
     headers = {
         'content-type': 'application/json; charset=utf-8',
         'Authorization': f"Bearer {OPENAI_API_KEY}"
     }
     data = {
+        'model': OPENAI_EMBEDDING_MODEL,
+        'input': chunk
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response_json = response.json()
+    embedding = response_json["data"][0]["embedding"]
+    return embedding
+
+
+def construct_llm_payload(question, context_chunks, chat_history):
+    # Build the prompt with the context chunks and user's query
+    prompt = build_prompt(question, context_chunks)
+    print("\n==== PROMPT ====\n")
+    print(prompt)
+
+    # Construct messages array to send to OpenAI
+    messages = construct_messages_list(chat_history, prompt)
+
+    # Construct headers including the API key
+    headers = {
+        'content-type': 'application/json; charset=utf-8',
+        'Authorization': f"Bearer {OPENAI_API_KEY}"
+    }
+
+    # Construct data payload
+    data = {
         'model': CHATGPT_MODEL,
         'messages': messages,
         'temperature': 1,
-        'max_tokens': 1000
+        'max_tokens': 1000,
+        'stream': True
     }
 
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    response_json = response.json()
-    completion = response_json["choices"][0]["message"]["content"]
-    return completion
+    return headers, data
--- app/utils/helper_functions.py
+++ app/utils/helper_functions.py
@@ -2,6 +2,7 @@
 
 PROMPT_LIMIT = 3750
 
+
 def chunk_text(text, chunk_size=200):
     # Split the text by sentences to avoid breaking in the middle of a sentence
     sentences = text.split('. ')
@@ -24,9 +25,10 @@ def chunk_text(text, chunk_size=200):
 
     return chunks
 
+
 def build_prompt(query, context_chunks):
     prompt_start = (
-            "Answer the question based on the context below. If you don't know the answer based on the context provided below, just respond with 'I don't know' instead of making up an answer. Return just the answer to the question, don't add anything else. Don't start your response with the word 'Answer:'. Make sure your response is in markdown format\n\n"+
+            "Answer the question based on the context below. If you don't know the answer based on the context provided below, just respond with 'I don't know' instead of making up an answer. Return just the answer to the question, don't add anything else. Don't start your response with the word 'Answer:'. Make sure your response is in markdown format\n\n" +
             "Context:\n"
     )
     prompt_end = (
@@ -39,14 +41,30 @@ def build_prompt(query, context_chunks):
         if len("\n\n---\n\n".join(context_chunks[:i])) >= PROMPT_LIMIT:
             prompt = (
                     prompt_start +
-                    "\n\n---\n\n".join(context_chunks[:i-1]) +
+                    "\n\n---\n\n".join(context_chunks[:i - 1]) +
                     prompt_end
             )
             break
-        elif i == len(context_chunks)-1:
+        elif i == len(context_chunks) - 1:
             prompt = (
                     prompt_start +
                     "\n\n---\n\n".join(context_chunks) +
                     prompt_end
             )
     return prompt
+
+
+def construct_messages_list(chat_history, prompt):
+    messages = [{"role": "system", "content": "You are a helpful assistant."}]
+
+    # Populate the messages array with the current chat history
+    for message in chat_history:
+        if message['isBot']:
+            messages.append({"role": "system", "content": message["text"]})
+        else:
+            messages.append({"role": "user", "content": message["text"]})
+
+    # Replace last message with the full prompt
+    messages[-1]["content"] = prompt
+
+    return messages
